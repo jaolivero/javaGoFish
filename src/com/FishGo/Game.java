494e394491fd780c31028accb4bc92eeb2cc7ab8
Joel Olivero <joelolivero@outlook.com>
2020-12-19 01:20:36 -0500

    added methods

--- src/com/FishGo/Game.java
+++ src/com/FishGo/Game.java
@@ -8,6 +8,8 @@ public class Game {
     public List<Player> allPlayers = new ArrayList<Player>();
     Deck deck = new Deck();
     Scanner scan = new Scanner(System.in);
+    Player cpu = new Player("CPU");
+    Player cpuTwo = new Player("CPU TWO");
 
     public Game () {
     }
@@ -16,10 +18,22 @@ public class Game {
         System.out.println("Enter your username");
         String username = scan.nextLine();
         Player myPlayer = new Player(username);
-        Hand myHand = new Hand(myPlayer);
-        deal(5, deck, myHand);
-        System.out.println(myPlayer.getUserName() + " Hand ");
-        System.out.println(myHand);
+        allPlayers.add(myPlayer);
+//        allPlayers.add(cpu);
+////        allPlayers.add(cpuTwo);
+
+        for (Player player: allPlayers)
+        {
+            Hand myHand = new Hand(myPlayer);
+            deal(5, deck, myHand);
+            System.out.println(player.getUserName());
+            System.out.println(myHand);
+            System.out.println();
+        }
+        System.out.println("Enter the number you want to pull from CPU's hand");
+        int choice = scan.nextInt();
+
+        // System.out.println(myPlayer.getUserName() + " Hand ");
     }
 
     public static void deal(int cardsDealt, Deck currentDeck, Hand currentHand) {
@@ -28,11 +42,10 @@ public class Game {
             Card gettingCard = currentDeck.getCard(0);
             currentDeck.removeCard(0);
             currentHand.addCard(gettingCard);
-            //System.out.println(gettingCard);
         }
     }
 
-    public void printScore(Hand myHand) {
+    public void printHand(Hand myHand) {
         System.out.println("SCORE");
         System.out.println("----");
         System.out.println(" " + myHand.getScore() + " ");
